89f91566ea16580770d0238f38bfbcd15e7a8f5e
Udodirim L. Agonsi <88558628+Agonsi123@users.noreply.github.com>
2024-06-21 08:41:30 +0100

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,33 +12,29 @@ const Header = () => {
                 <div className='size-9 md:size-full'>
                     <img src={image12} alt="" />
                 </div>
-                <div className='md:static absolute  md:min-h-fit min-h-[20vh] left-4 top-[8%] ml-20 mt-[-4.4rem] md:mt-1 md:w-auto flex items-center px-5 '>
-                    <ul className='flex md:flex-row  md:items-center md:gap-[9.2vw] gap-4'>
-                            <li>
-                                <NavLink to="/" exact
-                                activeClassName="active">
-                                    <a className='hover:text-blue-500 text-[8px] md:text-[12px]' href=''>MAIN</a>
+                <div className='md:static absolute  md:min-h-fit min-h-[20vh] left-5 top-[8%] ml-20 mt-[-4.4rem] md:mt-2 md:w-auto flex items-center px-5 '>
+                    <ul className='flex md:flex-row  md:items-center md:gap-[10.2vw] gap-4'>
+                            <li className='hover:text-blue-500 text-[8px] md:text-[12px]'>
+                                <NavLink to="/">
+                                    MAIN
                                 </NavLink>
                             </li>
-                            <li>
-                                <NavLink to="/gallery" exact
-                                activeClassName="active">
-                                    <a className='hover:text-blue-500 text-[8px] md:text-[12px]' href=''>GALLERY</a>
+                            <li className='hover:text-blue-500 text-[8px] md:text-[12px]'>
+                                <NavLink to="/gallery">
+                                    GALLERY
                                 </NavLink>
                             </li>
-                            <li>
-                                <NavLink to="/projects" exact
-                                activeClassName="active">
-                                    <a className='hover:text-blue-500 text-[8px] md:text-[12px]' href=''>PROJECTS</a>
+                            <li className='hover:text-blue-500 text-[8px] md:text-[12px]'>
+                                <NavLink to="/projects">
+                                    PROJECTS
                                 </NavLink>
                             </li>
                         {/* <li>
                             <a className='hover:text-blue-500' href='#'>CERTIFICATIONS</a>
                         </li> */}
-                            <li>
-                                <NavLink to="/contacts" exact
-                                activeClassName="active">
-                                    <a className='hover:text-blue-500 text-[8px] md:text-[12px]' href=''>CONTACTS</a>
+                            <li className='hover:text-blue-500 text-[8px] md:text-[12px]'>
+                                <NavLink to="/contacts">
+                                    CONTACTS
                                 </NavLink>
                             </li>
                     </ul>
